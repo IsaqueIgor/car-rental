74c3a8f27cb64bbd55fb37a24c303a596d027106
Isaque Igor <isaqueigorsilva@gmail.com>
2022-01-20 17:31:28 -0300

    feat: create Specification Repository

--- src/modules/cars/services/CreateSpecificationervice.ts
+++ src/modules/cars/services/CreateSpecificationervice.ts
@@ -1,7 +1,24 @@
-class CreateSpecificationervice  {
-  constructor() {}
+import { ISpecificationRepository } from "../repositories/ISpecificationRepository";
 
-  execute(): void{
+interface  IRequest {
+  name: string
+  description: string
+}
+
+class CreateSpecificationervice  {
+  constructor(private specificationRepository: ISpecificationRepository) {}
+
+  execute({name,description}: IRequest): void{
+    const specificationAlreadyExist = this.specificationRepository.findByName(name)
+
+    if(specificationAlreadyExist){
+      throw new Error('Specification already exists!')
+    }
+
+    this.specificationRepository.create({
+      description: description,
+      name: name
+    })
   }
 }
 
