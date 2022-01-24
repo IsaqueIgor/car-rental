a27b9d17d26984f20270104a0ad53feaeec846ce
Isaque Igor <isaqueigorsilva@gmail.com>
2022-01-23 22:35:01 -0300

    feat: implement multer

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+
 import { router } from './routes'
 
 const PORT = 3333
@@ -8,6 +9,5 @@ const app = express()
 app.use(express.json())
 
 app.use(router)
-
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`))
