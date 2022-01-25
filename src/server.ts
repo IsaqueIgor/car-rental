70e71cb4a16998d4f4938684f4c0a3f20ada9299
Isaque Igor <isaqueigorsilva@gmail.com>
2022-01-25 15:00:32 -0300

    chore: config documentation on swagger

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+
+import swaggerFile from './swagger.json'
 
 import { router } from './routes'
 
@@ -7,6 +10,8 @@ const PORT = 3333
 const app = express()
 
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router)
 
